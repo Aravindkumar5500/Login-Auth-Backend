8d1b21eb6d1792bfdfc48d16cc7c9455d431413a
Aravindkumar5500 <aravindrps75@gmail.com>
2025-08-15 22:50:08 +0530

    add

---
+++ model/user.js
@@ -0,0 +1,30 @@
+const mongoose =require("mongoose")
+
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    email : {
+        type:String,
+        unique:true,
+        required:true,
+        lowercase:true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    otp:{
+        type:String,
+         default: null,
+    },
+    otpExpires:{
+        type:Date,
+        default: null,
+    }
+});
+
+module.exports=mongoose.model("User",UserSchema)
